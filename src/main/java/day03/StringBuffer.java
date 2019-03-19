46039fbcd5f6fc508da01c7bb50460bb32a4b460
chenyuefang <416326391@qq.com>
2019-03-19 09:28:11 +0800

    create demo

--- src/main/java/day03/StringBuffer.java
+++ src/main/java/day03/StringBuffer.java
@@ -3,9 +3,17 @@ package day03;
 public class StringBuffer {
 
     public static void main(String[] args) {
+
+        String s="hello";
         StringBuffer stringBuffer = new StringBuffer(); //
+
+        String string=stringBuffer.toString();
+
 
         // String -> StringBuffer
         // StringBuffer -> String
+
+        // String 不可变的 降低运行速度
+        // StringBuffer 可变的
     }
 }
