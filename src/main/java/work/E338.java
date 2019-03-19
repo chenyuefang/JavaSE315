a52caafc991b2b06d666577f19c3665790b3126c
chenyuefang <416326391@qq.com>
2019-03-19 16:56:54 +0800

    update

--- src/main/java/work/E338.java
+++ src/main/java/work/E338.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.TreeSet;
 
 /*
+有两个List<Integer>, 写一个方法要求合并这两个集合，不能有重复，
+不能用集合的sort等方法，而且要求中间的数字最大，两边逐渐减小。
+例如
+(m 是集合的 size)
+（ get(0) < get(m-1) , get(1) < get(m-2)）
 
  */
 public class E338 {
