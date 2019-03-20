2361111e54d0cceeef3acbcbaeb9d5aabfa4816b
chenyuefang <416326391@qq.com>
2019-03-20 11:13:31 +0800

    update

---
+++ src/main/java/day04/InterfaceTest.java
@@ -0,0 +1,18 @@
+package day04;
+
+public class InterfaceTest <T,S,U> {
+
+    T method(T t, S s, U u);
+}
+
+abstract class GenericClass<T> {
+
+    abstract void method(T t);
+}
+
+class SubGenericClass<T> extends GenericClass<T> { //
+    @Override
+    void method(T t) {
+
+    }
+}
