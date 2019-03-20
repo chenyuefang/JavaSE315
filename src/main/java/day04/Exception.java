feb0e8c2e3ca60961cf8f5b185d84a5dd9085331
chenyuefang <416326391@qq.com>
2019-03-20 17:02:12 +0800

    update

--- src/main/java/day04/Exception.java
+++ src/main/java/day04/Exception.java
@@ -11,7 +11,11 @@ public class Exception extends Throwable {
             System.out.println("hello".charAt(5));
             System.out.println(new int[]{1, 2, 3}[3]);
             System.out.println(Integer.parseInt("l23"));
-        } catch (ArithmeticException e) {  // 后面跟多个catch
+        }
+        /*catch (Exception e) {
+            System.out.println("Exception");
+        }
+        */catch (ArithmeticException e) {  // 后面跟多个catch
             System.out.println("ArithmeticException...");
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("StringIndexOutOfBoundsException");
@@ -20,11 +24,6 @@ public class Exception extends Throwable {
         } catch (NumberFormatException e) {
             System.out.println("NumberFormatException");
         }
-
-        /*catch (Exception e) {
-            System.out.println("Exception");
-        }
-        */
         finally { // 只能有一个finally 可以没有
             System.out.println("finally...");
         }
@@ -33,6 +32,23 @@ public class Exception extends Throwable {
     }
 
 }
+
+
+/*
+Try  {…}
+Catch (ClassCastException ex) {………}
+Catch (NumberFormatException ex) {………}
+Catch (Exception ex) {………}  // 此句必须放在最后！
+
+
+Exception是异常的父类。如果没有把该语句Catch (Exception ex) {………}放在最后，
+那么try 块中产生的异常不可能被Catch (Exception ex) {………}之后的catch语句捕获，
+也就是说Catch (Exception ex) {………}之后的catch语句永远都不会执行。
+Java编译器不允许出现这样的情况，就会出现编译错误.
+因此如果要捕获的异常有继承关系，则子类在前，父类在后
+ */
+
+
 /* 只捕获第一个空指针异常，其他的不再执行
 输出    ArithmeticException...
         finally...
