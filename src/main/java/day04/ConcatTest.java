ed001eb85bb043b6f12e743ead5f409f1a20749c
chenyuefang <416326391@qq.com>
2019-03-20 11:23:12 +0800

    update

--- src/main/java/day04/ConcatTest.java
+++ src/main/java/day04/ConcatTest.java
@@ -24,7 +24,7 @@ public class ConcatTest<T> { //T :类型参数   Type Parameters
     public static void main(String[] args) {
         String s1 = "Tom,";
         String s2 = "Jerry";
-        ConcatTest<String> c1 = new ConcatTest<>();
+        ConcatTest<String> c1 = new ConcatTest<>(); // 字符串封装类型
         c1.concat(s1, s2);
 
         int i1 = 1;
@@ -36,5 +36,28 @@ public class ConcatTest<T> { //T :类型参数   Type Parameters
         boolean b2 = false;
         ConcatTest<Boolean> c3 = new ConcatTest<>();
         c3.concat(b1, b2);
+
+        Integer integer = new Integer(100);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        int i = 100;
+        String s = String.valueOf(i); // i + "";  // 整形转化成字符串
+
+        String s100 = "100";
+        int i100 = Integer.parseInt(s100);   // 字符串转化成整形
+
+        System.out.println(s);
+        System.out.println(i100 + 1);
+
+        String s12345 = "123.45";
+        double d = Double.parseDouble(s12345);  // 字符串转化成双精度
+
+        Boolean.parseBoolean("true");  // 布尔类型的转化，不常用
+
     }
 }
