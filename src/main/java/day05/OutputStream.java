0ddc1cd61d5c9e265252c73d2a858e13781fa196
chenyuefang <416326391@qq.com>
2019-03-21 11:05:32 +0800

    update

--- src/main/java/day05/OutputStream.java
+++ src/main/java/day05/OutputStream.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class OutputStreamTest {
+class OutputStreamTest {
     public static void main(String[] args) {
         try(OutputStream outputStream = new FileOutputStream("output_stream.txt")) {
             outputStream.write(97);
