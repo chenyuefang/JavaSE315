11c3a23f961f375c53502ddb0e4b472afec9e344
chenyuefang <416326391@qq.com>
2019-03-21 11:42:14 +0800

    update

---
+++ src/main/java/work/E377.java
@@ -0,0 +1,30 @@
+package work;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+/**
+ * 写一个方法，输入一个文件名各一个字符串，统计这个字符串在这个文件中出现的次数
+ */
+
+public class E377 {
+
+    public int countWords(String  file, String find) throws Exception {
+        int count = 0;
+        Reader in = new FileReader(file);
+        int c ;
+        while ((c = in.read()) != -1) {
+            while (c == find.charAt(0)) {
+                for (int i = 1; i < find.length(); i++) {
+                    c = in.read();
+                    if (c != find.charAt(i))
+                        break;
+                    if (i == find.length() - 1)
+                        count++;
+
+                }
+            }
+        }
+        return count;
+    }
+}
