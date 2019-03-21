106c25153b6ed5c9f740be55446e928562b8e160
chenyuefang <416326391@qq.com>
2019-03-21 11:12:30 +0800

    update

--- src/main/java/day05/ReaderTest.java
+++ src/main/java/day05/ReaderTest.java
@@ -11,6 +11,11 @@ import java.io.Reader;
 public class ReaderTest {
     public static void main(String[] args) {
         try (Reader reader = new FileReader("src/main/java/day05/ReaderTest.java");) {
+            /**
+             * try()内的内容是需要关闭的资源
+             * 若有多条资源，除最后一句外，其他语句都需要加“；”
+             */
+
             int i = reader.read();
             while (i != -1) {
                 System.out.print((char) i);
