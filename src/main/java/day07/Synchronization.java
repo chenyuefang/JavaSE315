e2ef6b5c6c6aa87f34ff3ab5c320ca2266cf90ab
chenyuefang <416326391@qq.com>
2019-03-25 14:50:21 +0800

    update

---
+++ src/main/java/day07/Synchronization.java
@@ -0,0 +1,31 @@
+package day07;
+
+public class Synchronization {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Food {
+
+}
+
+class Cat extends Thread {
+
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class Dog extends Thread {
+
+
+    @Override
+    public void run() {
+
+    }
+
+}
