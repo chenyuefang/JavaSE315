f717c80f938a45f05dfa38168f07dadf45691d83
chenyuefang <416326391@qq.com>
2019-03-25 08:26:00 +0800

    update

--- src/main/java/Exercise/ex1.java
+++ src/main/java/Exercise/ex1.java
@@ -1,6 +1,7 @@
 package Exercise;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * 编写一个程序，在D盘根目录下创建一个以自己名称命名的文件夹，
@@ -10,7 +11,14 @@ import java.io.File;
 public class ex1 {
 
     public static void main(String[] args) {
-        File file=new File("CYF");
+        File file=new File("CYF");// 只有一个文件名
+        try {
+           file.createNewFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(file.getPath());
         System.out.println(file.getName());
     }
 }
