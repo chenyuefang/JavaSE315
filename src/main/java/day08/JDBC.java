b9216e6cc7f260a709e9f8808969ddef8bc70d0d
chenyuefang <416326391@qq.com>
2019-03-26 17:05:09 +0800

    update

---
+++ src/main/java/day08/JDBC.java
@@ -0,0 +1,43 @@
+package day08;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+//  JDBC - Java DataBase Connectivity
+
+import java.sql.Driver;
+
+public class JDBC {
+
+    private static final String URL="jdbc:mysql://127.0.0.1:3306";
+    private static final String User="root";
+    private static final String PASSWORD="123456";
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+         // 1.Driver
+      //  new Driver();
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.Connection
+        Connection connection = DriverManager.getConnection(URL,User,PASSWORD);
+
+        //3.PrepareStatement
+        String sql = "insert into db_test.user value (null,?,md5(?))";
+        PreparedStatement preparedStatement =connection.prepareStatement(sql);
+        preparedStatement.setString(1,"Tom");
+        preparedStatement.setString(2, "123");
+
+        // 4.1 p.executeUpdate(); // DML
+        preparedStatement.executeUpdate();
+
+        // 4.2 p.executeQuery(); // DQL
+
+        //5. *.close();
+        preparedStatement.close();
+        connection.close();
+        System.out.println("done.");
+    }
+
+
+}
