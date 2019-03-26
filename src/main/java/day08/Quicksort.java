9a95b4b14c2641e1d85fb0e6d0200ca7e77795bc
chenyuefang <416326391@qq.com>
2019-03-26 11:45:48 +0800

    update

--- src/main/java/day08/Quicksort.java
+++ src/main/java/day08/Quicksort.java
@@ -1,33 +1,45 @@
 package day08;
 
+import java.util.Arrays;
+
 public class Quicksort {
 
+    public static void Quicksort(int low, int high) {
 
-    /**
-     *  int low;
-     *         int high;
-     *         //已经排完
-     *         if (low >= high) {
-     *             return;
-     *         }
-     *         int left = low;
-     *         int right = high;
-     *
-     *         //保存基准值
-     *         int pivot = ints[left];
-     *         while (left < right) {
-     *             //从后向前找到比基准小的元素
-     *             while (left < right && ints[right] >= pivot)
-     *                 right--;
-     *             ints[left] = ints[right];
-     *             //从前往后找到比基准大的元素
-     *             while (left < right && ints[left] <= pivot)
-     *                 left++;
-     *             ints[right] = ints[left];
-     *         }
-     *         // 放置基准值，准备分治递归快排
-     *         ints[left] = pivot;
-     *         sort(a, low, left - 1);
-     *         sort(a, left + 1, high);
-     */
+        int[] int3 = new int[10];
+        for (int i = 0; i < int3.length; i++) {
+            int3[i] = (int) (Math.random() * 100);// 数组中随机产生10个数
+        }
+        System.out.println(Arrays.toString(int3)); // 输出随机的数组
+        // Arrays.toString(ints) 字符串形式的数组
+
+
+        //已经排完
+        if (low >= high) {
+            return;
+        }
+        int left = low;
+        int right = high;
+
+        //保存基准值
+        int pivot = int3[left];
+        while (left < right) {
+            //从后向前找到比基准小的元素
+            while (left < right && int3[right] >= pivot)
+                right--;
+            int3[left] =int3[right];
+            //从前往后找到比基准大的元素
+            while (left < right &&int3[left] <= pivot)
+                left++;
+            int3[right] = int3[left];
+        }
+        // 放置基准值，准备分治递归快排
+        int3[left] = pivot;
+        Quicksort(int3, low, left - 1);
+        Quicksort(int3, left + 1, high);
+
+        System.out.println(Arrays.toString(int3));
+    }
+
+
 }
--- src/main/java/day08/SimpleSort.java
+++ src/main/java/day08/SimpleSort.java
@@ -1,20 +1,67 @@
 package day08;
 
+import java.util.Arrays;
+
 public class SimpleSort {
-    /**
-     for (int i = 0; i < ints.length; i++) {
+
+    public static void main(String[] args) {
+
+        int[] int2 = new int[10];
+        for (int i = 0; i < int2.length; i++) {
+            int2[i] = (int) (Math.random() * 100);// 数组中随机产生10个数
+        }
+        System.out.println(Arrays.toString(int2)); // 输出随机的数组
+        // Arrays.toString(ints) 字符串形式的数组
+
+     for (int i = 0; i < int2.length; i++) {
      int min = i;
-     for (int j = i + 1; j < ints.length; j++) {
-     if (ints[j] < ints[i]) {
-     min = ints[j];
+     for (int j = i + 1; j < int2.length; j++) {
+     if (int2[j] < int2[i]) {
+     min = int2[j];
      }
      }  //  选出之后待排序中值最小的位置
      if (min != i) {
-     int temp = ints[i];
-     ints[i] = ints[min];
-     ints[min] = temp;
+     int temp = int2[i];
+     int2[i] = int2[min];
+     int2[min] = temp;
      }  // 只要小于之前被选出位置对应的值，就交换
      }    // 简单选择排序
 
-     */
+
+        System.out.println(Arrays.toString(int2));// 输出排序后的数组
 }
+
+}
+
+
+/**
+ * public class SimpleSort{
+ *     public static void sort(Comparable[] data){
+ *         //数组长度
+ *         int len=data.length;
+ *         for(int i=0; i<len; i++){
+ *             //记录当前位置
+ *             int position = i;
+ *             //找出最小的数，并用position指向最小数的位置
+ *             for(int j=i+1; j<len; j++){
+ *                 if( data[position].compareTo(data[j]) > 0 ) {
+ *                     position=j;
+ *                 }//endif
+ *             }//endfor
+ *             //交换最小数data[position]和第i位数的位置
+ *             Comparable temp=data[i];
+ *             data[i]=data[position];
+ *             data[position]=temp;
+ *         }//endfor
+ *     }//endsort
+ *     public static void main(String[] args) {
+ *         //在JDK1.5版本以上,基本数据类型可以自动装箱
+ *         //int,double等基本类型的包装类已实现了Comparable接口
+ *         Comparable[] c={4,9,23,1,45,27,5,2};
+ *         sort(c);
+ *         for(Comparable data:c) {
+ *             System.out.println(data);
+ *         }
+ *     }
+ * }
+ */
