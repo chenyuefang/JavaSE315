8ecabc392f0d2c1bab9a1d227b9ae0159001ee44
chenyuefang <416326391@qq.com>
2019-03-26 09:21:42 +0800

    update

---
+++ src/main/java/day08/FibonacciTest.java
@@ -0,0 +1,11 @@
+package day08;
+
+/**
+ * 斐波那契 Fibonacci 数列：0, 1, 1, 2, 3, 5, 8..., 求f(n)
+ */
+
+public class FibonacciTest {
+
+
+
+}
---
+++ src/main/java/day08/HanoiTower.java
@@ -0,0 +1,18 @@
+package day08;
+
+import java.util.Scanner;
+
+public class HanoiTower {
+
+    private static void Hanoi(int n) {
+// recursive call
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("imput number N:");
+        int n = scanner.nextInt();
+
+        Hanoi(n);
+    }
+}
